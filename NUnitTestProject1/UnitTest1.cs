0b6f51b943bf81fbf452c7dfd029445d3598d984
dmziryanov <dmziryanov@yandex.ru>
2021-03-22 16:02:01 +0300

    юнит тесты

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,18 +1,34 @@
+using ConsoleApp2;
 using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace NUnitTestProject1
 {
     public class Tests
     {
+        MultivariableDictionary<string, string> mv = new MultivariableDictionary<string, string>();
+
         [SetUp]
         public void Setup()
         {
+            mv.Add("a", "b");
+            mv.Add("a", "c");
+            mv.Add("a", "c");
         }
 
         [Test]
-        public void Test1()
+        public void TestQuantity()
         {
-            Assert.Pass();
+            var mv = new MultivariableDictionary<string, string>();
+            Assert.AreEqual(2,  mv.Count());
+        }
+
+        public void SequenceEqual()
+        {
+            List<string> l = new List<string>() { "a", "b" };
+            Assert.IsTrue(mv.ToList().SequenceEqual<string>(l));
         }
     }
 }
